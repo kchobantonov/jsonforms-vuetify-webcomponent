3c9c66ddb1ce7d8b5dc3d66de134c9596f576dfa
Krasimir Chobantonov <kchobantonov@gmail.com>
2024-11-16 01:14:57 -0500

    clean up generated html

--- packages/example/src/components/VuetifyJsonFormsWrapper.vue
+++ packages/example/src/components/VuetifyJsonFormsWrapper.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, useAttrs } from 'vue';
 import { useScriptTag } from '@vueuse/core';
 
 const location = './js/vuetify-json-forms.js';
 const loading = ref(true);
+
+// Disable automatic inheritance of attributes
+defineOptions({
+  inheritAttrs: false,
+});
+
+// Get $attrs manually
+const attrs = useAttrs();
 
 useScriptTag(
   location,
@@ -16,13 +24,11 @@ useScriptTag(
 </script>
 
 <template>
-  <div>
-    <v-progress-linear
-      v-if="loading"
-      indeterminate
-      rounded
-      height="6"
-    ></v-progress-linear>
-    <vuetify-json-forms v-else v-bind="$attrs"></vuetify-json-forms>
-  </div>
+  <v-progress-linear
+    v-if="loading"
+    indeterminate
+    rounded
+    height="6"
+  ></v-progress-linear>
+  <vuetify-json-forms v-else v-bind="attrs"></vuetify-json-forms>
 </template>
