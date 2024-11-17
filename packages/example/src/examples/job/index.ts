8404a8bfc08a9ac9ef701def9359abeb0a9e8434
Krasimir Chobantonov <kchobantonov@gmail.com>
2024-11-17 03:52:04 -0500

    update example

--- packages/example/src/examples/job/index.ts
+++ packages/example/src/examples/job/index.ts
@@ -1,5 +1,6 @@
 import type { ExampleInputDescription } from '@/core/types';
 import data from './data.json';
+import i18n from './i18n.json';
 import schema from './schema.json';
 import uischema from './uischema.json';
 import uischemas from './uischemas.json';
@@ -8,6 +9,7 @@ import { NOT_APPLICABLE } from '@jsonforms/core';
 export const input: ExampleInputDescription = {
   schema,
   uischema,
+  i18n,
   uischemas: uischemas.map((item) => ({
     ...item,
     tester: (jsonSchema, schemaPath, path) => {
